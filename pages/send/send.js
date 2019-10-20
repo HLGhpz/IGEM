19519694469bcaf8529693b3c2b89aea411eb7b5
hlghpz <1090377416@qq.com>
2019-10-20 19:19:42 +0800

    完成功能

--- pages/send/send.js
+++ pages/send/send.js
@@ -11,7 +11,7 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         console.log(options)
         this.setData({
             deviceName: options.name,
@@ -22,14 +22,14 @@ Page({
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
         //停止搜寻附近的蓝牙外围设备
         wx.showLoading({
             title: 'Connect...',
@@ -40,39 +40,39 @@ Page({
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     },
 
-    Start_recording: function(e) {
+    Start_recording: function (e) {
         console.log(e);
         let that = this;
         if (this.data.ConnectionStatus == false) {
@@ -95,14 +95,16 @@ Page({
 
     },
 
-    View_status: function(e) {
+    View_status: function (e) {
         console.log(e);
+        // const value = 'd\r\n';
+        // that.writeBLECharacteristicValue(value)
         wx.navigateTo({
             url: '../cam/cam',
         })
     },
 
-    Reproduce: function(e) {
+    Reproduce: function (e) {
         console.log(e);
         let that = this;
         if (this.data.ConnectionStatus == false) {
@@ -122,7 +124,7 @@ Page({
         }
     },
 
-    Erase: function(e) {
+    Erase: function (e) {
         console.log(e);
         let that = this;
         if (this.data.ConnectionStatus == false) {
@@ -251,7 +253,7 @@ Page({
             serviceId: that.data.serviceId,
             characteristicId: that.data.writecharacteristics,
             value: buffer,
-            success: function(res) {
+            success: function (res) {
                 console.log("成功", res)
             },
         })
